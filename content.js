eb3982587491a2d8cdd4d5014f5d6b9f276a341d
Brad Vorjohan <bhvorjo@gmail.com>
2020-07-11 19:53:36 -0400

    initial commit

---
+++ content.js
@@ -0,0 +1,5 @@
+const whatsNew = document.querySelector(".css-1dbjc4n.r-1ysxnx4.r-k0dy70.r-1ylenci.r-1phboty.r-rs99b7.r-dr54s0.r-1udh08x")
+whatsNew.style.cssText = "display:none"
+const whoToFollow = document.querySelector(".css-1dbjc4n.r-1ysxnx4.r-k0dy70.r-1ylenci.r-1phboty.r-rs99b7.r-dr54s0.r-1bro5k0.r-1udh08x")
+whoToFollow.style.cssText = "display:none"
+
