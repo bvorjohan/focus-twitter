cad861dfcfdc6d7b0fff4c2dd4aebb18a0b9df5c
Brad Vorjohan <bhvorjo@gmail.com>
2020-07-11 22:23:26 -0400

    testing complete

--- content.js
+++ content.js
@@ -1,5 +1,29 @@
-const whatsNew = document.querySelector(".css-1dbjc4n.r-1ysxnx4.r-k0dy70.r-1ylenci.r-1phboty.r-rs99b7.r-dr54s0.r-1udh08x")
-whatsNew.style.cssText = "display:none"
-const whoToFollow = document.querySelector(".css-1dbjc4n.r-1ysxnx4.r-k0dy70.r-1ylenci.r-1phboty.r-rs99b7.r-dr54s0.r-1bro5k0.r-1udh08x")
-whoToFollow.style.cssText = "display:none"
+let whatsNewIntervalId = null;
+let whoToFollowIntervalId = null;
+let whatsNew = null;
+let whoToFollow = null;
+ 
+
+const clearItem = (nodes, intervalToClear) => {
+    nodes.forEach((item) => item.remove())
+    // item.style.cssText = "display:none";
+    clearInterval(intervalToClear)
+}
+
+
+
+(() => { 
+
+    whatsNewIntervalId = setInterval(() => {
+        whatsNew = document.querySelectorAll(".css-1dbjc4n.r-1ysxnx4.r-k0dy70.r-1ylenci.r-1phboty.r-rs99b7.r-dr54s0.r-1udh08x")
+        if (!!whatsNew) {
+            clearItem(whatsNew,whatsNewIntervalId)
+        }
+    }, 1000)
+
+    
+})()
+
+
+
 
